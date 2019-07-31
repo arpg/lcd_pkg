18f3fe202802c63064b68f305e7bbdd6b66b7ec8
shakeebbb <36937615+shakeebbb@users.noreply.github.com>
2019-07-31 14:10:53 -0600

    Updated lcd_node to call the service trajectory_query and publish the pose array and number of loop closures in the poseGraph message

--- src/artifact_update.cpp
+++ src/artifact_update.cpp
@@ -2,8 +2,12 @@
 #include "ros/package.h"
 #include "cartographer_ros_msgs/TrajectoryQuery.h"
 #include "geometry_msgs/Point.h"
-#include "lcd_pkg/poseGraph.h"
-#include "std_msgs/UInt32.h"
+#include "geometry_msgs/TransformStamped.h"
+#include "Transform.h"
+#include "marble_artifact_detection_msgs/ArtifactArray.h"
+#include "lcd_pkg/ArtifactStamped.h"
+#include "std_msgs/Bool.h"
+#include "std_msgs/Int32.h"
 #include "math.h"
 #include <iostream>
 #include <fstream>
@@ -11,18 +15,136 @@
 
 using namespace std;
 
-ros::Publisher poseGraphPub;
 int arraySize_param = -1;
 double mseThreshold_param = -1;
-double publish_rate = -1;
+bool loopClosureFlag = false;
 ofstream lout;
 
+void artifactCb(const marble_artifact_detection_msgs::ArtifactArray&);
 void trajCb(const vector<geometry_msgs::PoseStamped>&);
 float mse(vector<geometry_msgs::Point>&, vector<geometry_msgs::Point>&);
 int sc(vector<geometry_msgs::Point>&, vector<geometry_msgs::Point>&);
 int zc(vector<geometry_msgs::Point>&);
 void initializeLogs(string, string);
 
+class ArtifactArray
+{
+	vector <uint> poseGraphIndex;
+	vector <tf::Transform> world2robot;
+	vector <tf::Transform> robot2artifact;
+	vector <tf::Transform> world2artifact;
+	vector <string> objectClass;
+	
+	public:
+	
+	int size() // Returns artifact array size
+	{
+		return poseGraphIndex.size();
+	}
+	
+	void clear()
+	{
+		poseGraphIndex.clear();
+		world2robot.clear();
+		robot2artifact.clear();
+		world2artifact.clear();
+		objectClass.clear();
+	}
+	
+	void populate(vector <marble_artifact_detection_msgs:ArtifactArray>& artifactArray, vector<geometry_msgs::Pose>& poseGraph, ArtifactArray&); // Populates the array with the incoming information
+	{
+		clear();
+		
+		for (int i = 0; i < artifactArray.base_poses.size().size(); i++)
+		{
+			poseGraphIndex.push_back(artifactArray.base_pose_indices[i]);
+			
+			tf::Transform transform;
+			
+			tf::Vector3 translation_world2robot(poseGraph[artifactArray.base_pose_indices[i]].pose.position.x,
+																					poseGraph[artifactArray.base_pose_indices[i]].pose.position.y,
+																					poseGraph[artifactArray.base_pose_indices[i]].pose.position.z);
+			tf::Quaternion rotation_world2robot(poseGraph[artifactArray.base_pose_indices[i]].pose.orientation.x,
+																					poseGraph[artifactArray.base_pose_indices[i]].pose.orientation.y,
+																					poseGraph[artifactArray.base_pose_indices[i]].pose.orientation.z,
+																					poseGraph[artifactArray.base_pose_indices[i]].pose.orientation.w);
+			
+			transform.setOrigin(translation_world2robot);
+			transform.setRotation(rotation_world2robot);
+			world2robot.push_back(transform);
+			
+			
+			tf::Vector3 translation_robot2artifact(artifactArray.artifacts[i].position.x - poseGraph(artifactArray.base_pose_indices[i]).pose.position.x,
+																						 artifactArray.artifacts[i].position.y - poseGraph(artifactArray.base_pose_indices[i]).pose.position.y,
+																						 artifactArray.artifacts[i].position.z - poseGraph(artifactArray.base_pose_indices[i]).pose.position.z);
+			tf::Quaternion rotation_robot2artifact(0, 0, 0, 1);	
+			
+			transform.setOrigin(translation_robot2artifact);
+			transform.setRotation(rotation_robot2artifact);
+			robot2artifact.push_back(transform);																	 
+			
+			transform.mult(world2robot.back(), robot2artifact.back());
+			world2artifact.push_back(transform);
+			
+			objectClass.push_back(artifacts[i].obj_class);
+			
+			prune(size()-1, aRadius);
+		}
+	}
+	
+	void prune(int artifactArrayIndex, float aRadius); // Keeps one if multiple artifacts are found at the same location
+	{
+		for (int i = 0; i < size(); i++)
+		{
+			if(i != artifactArrayIndex && objectClass[i] == objectClass[artifactArrayIndex] && distance(world2artifact[i], world2artifact[artifactArrayIndex]) < aRadius )
+			{
+				poseGraphIndex.erase(poseGraphIndex.begin()+artifactArrayIndex);
+				world2robot.erase(world2robot.begin()+artifactArrayIndex);
+				robot2artifact.erase(robot2artifact.begin()+artifactArrayIndex);
+				world2artifact.erase(world2artifact.begin()+artifactArrayIndex);
+				objectClass.erase(objectClass.begin()+artifactArrayIndex);
+			}
+		}
+	}
+	
+	float distance(tf::Transform transform1, tf::Transform transform2)
+	{
+		d = pow(transform1.getOrigin().getX()-transform2.getOrigin().getX(),2) + 
+				pow(transform1.getOrigin().getY()-transform2.getOrigin().getY(),2) + 
+				pow(transform1.getOrigin().getZ()-transform2.getOrigin().getZ(),2);
+		
+		return sqrt(d);
+	}
+	
+	void publish()
+	{
+		lcd_pkg::ArtifactStamped artifactStamped;
+		lcd_pkg::Artifact artifact;
+		
+		ros::Publisher artifactArrayPub = nh.advertise<lcd_pkg::ArtifactStamped>("artifact_array", 100);
+		
+		for (int i = 0; i < size(); i++)
+		{
+			artifact.poseGraphIndex = poseGraphIndex[i];
+			
+			artifact.objectClass(objectClass[i]);
+			
+			artifact.transform.translation.x = world2artifact[i].translation.x;
+			artifact.transform.translation.y = world2artifact[i].translation.y;
+			artifact.transform.translation.z = world2artifact[i].translation.z;
+			
+			artifact.transform.rotation.x = world2artifact[i].rotation.x;
+			artifact.transform.rotation.y = world2artifact[i].rotation.y;
+			artifact.transform.rotation.z = world2artifact[i].rotation.z;
+			artifact.transform.rotation.w = world2artifact[i].rotation.w;
+			
+			ArtifactStamped.artifact.push_back(artifact);
+		}
+		
+		artifactArrayPub.publish(ArtifactStamped);
+	}
+	
+};
 
 int main(int argc, char **argv)
 {
@@ -30,27 +152,26 @@ int main(int argc, char **argv)
 
   ros::NodeHandle nh;
 
-	poseGraphPub = nh.advertise<lcd_pkg::poseGraph>("pose_graph", 100);
+  ros::Publisher loopClosurePub = nh.advertise<std_msgs::Int32>("number_of_loop_closures", 100);
+  ros::Subscriber artifactArraySub = nh.subscribe("/H01/artifact_array", 10, artifactCb); 
   ros::ServiceClient client = nh.serviceClient<cartographer_ros_msgs::TrajectoryQuery>("trajectory_query");
   
-  while(arraySize_param == -1 || mseThreshold_param == -1 || publish_rate == -1)
+  while(arraySize_param == -1 || mseThreshold_param == -1)
   {
   ros::param::get("lcd_node/arraySize_param", arraySize_param);
   ros::param::get("lcd_node/mseThreshold_param", mseThreshold_param);
-  ros::param::get("lcd_node/publish_rate", publish_rate);
   
   ROS_INFO("Waiting for Parameters");
   }
   
-  initializeLogs("lcd_pkg", "lcd_logs.txt");
+  initializeLogs("lcd_pkg", "manuever_logs.txt");
   lout << "arraySize_param = " << arraySize_param << endl;
-  lout << "mseThreshold_param = " << mseThreshold_param << endl;
-  lout << "publish_rate = " << publish_rate << endl << endl;
+  lout << "mseThreshold_param = " << mseThreshold_param << endl << endl;
   
   cartographer_ros_msgs::TrajectoryQuery srv;
   srv.request.trajectory_id = 0;
 
-	ros::Rate loop_rate(publish_rate);
+	ros::Rate loop_rate(200);
 	
 	while(ros::ok())
 	{
@@ -58,8 +179,6 @@ int main(int argc, char **argv)
 		{
 			cout << "Trajectory status = " << srv.response.status.message << endl; 
 		  cout << "Trajectory size = " << srv.response.trajectory.size() << endl;
-		  
-		  if(srv.response.trajectory.size() > 0)
 		  cout << "Current Odometry: position (" << srv.response.trajectory.back().pose.position.x << ", "
 		  																			 << srv.response.trajectory.back().pose.position.y << ", "
 		  																			 << srv.response.trajectory.back().pose.position.z << ") "
@@ -83,9 +202,26 @@ int main(int argc, char **argv)
   return 0;
 }
 
+void artifactCb(const marble_artifact_detection_msgs::ArtifactArray& msg)
+{
+	static ArtifactArray artifactArray;
+	
+	if(msg.num_artifacts == 0)
+	return;
+	// Check for new values
+	// Split transforms and copy the new content
+	// Update the world2robot transforms for the artifactArray from the pose graph
+	// Recalculate the world2artifact transforms for the artifactArray
+	// Compare new content with the old content for similarity
+	
+	for(int i = 0; i < msg.artifacts.size(); i++)
+	{
+		artifactArray.populate(msg.artifacts, artifactArray);
+	}
+}
+
 void trajCb(const vector<geometry_msgs::PoseStamped>& msg)
-{	
-	static uint32_t nLoopClosures = 0;
+{
 	static bool isPopulated_prevTruncatedArray = false;
 	
 	const int truncatedArraySize = arraySize_param; // Number of array elements to compare
@@ -143,7 +279,7 @@ void trajCb(const vector<geometry_msgs::PoseStamped>& msg)
 		{
 			lout << "Loop Closure Detected" << endl;
 			cout << "Loop Closure Detected" << endl;
-			nLoopClosures = nLoopClosures + 1;
+			loopClosureFlag = true;
 			//getchar();
 		}
 	}
@@ -163,14 +299,7 @@ void trajCb(const vector<geometry_msgs::PoseStamped>& msg)
 	lout << "prevTruncatedArray populated" << endl;
 	prevInputArraySize = newInputArraySize;
 	lout << "prevInputArraySize reset to newInputArraySize" << endl;
-	
-	lout << "Publishing number of loop closures and the recent pose array" << endl;
-	lcd_pkg::poseGraph poseGraph;
-	poseGraph.nLoopClosures = nLoopClosures;
-	poseGraph.poseArray = msg;
-	poseGraphPub.publish(poseGraph);
 	lout << "<====================================>" << endl << endl;
-	
 }
 
 float mse(vector<geometry_msgs::Point>& array1, vector<geometry_msgs::Point>& array2)
